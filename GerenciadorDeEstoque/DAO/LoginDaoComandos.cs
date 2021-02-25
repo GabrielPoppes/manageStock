540d9640a8d43206bea8dddac5c46344dc12f0ec
Gabriel Poppes <gabrielpoppes@outlook.com>
2021-02-25 12:16:31 -0300

    Adicionada constraint unique na coluna email e celular, agora não pode haver cadastrado onde email ou celular já estejam armazenados no BD (duplicidade)

--- GerenciadorDeEstoque/DAO/LoginDaoComandos.cs
+++ GerenciadorDeEstoque/DAO/LoginDaoComandos.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GerenciadorDeEstoque.DAO
 {
@@ -35,7 +36,7 @@ namespace GerenciadorDeEstoque.DAO
                 armazenardados.Close();
             }
 
-            catch(SqlException)
+            catch (SqlException)
             {
                 this.mensagem = "Erro com banco de dados!";
             }
@@ -44,29 +45,51 @@ namespace GerenciadorDeEstoque.DAO
 
         public string Cadastrar(string email, string senha, string confirmarsenha, string celular, string lembretesenha)
         {
-            if (senha.Equals(confirmarsenha))
+            // checar se campos não estão em branco
+            if (!email.Equals("") && !senha.Equals("") && !celular.Equals("") && !lembretesenha.Equals(""))
             {
-                comando.CommandText = "insert into funcionario(email, senha, celular, lembretesenha)values(@email, @senha, @celular, @lembretesenha);";
-                comando.Parameters.AddWithValue("@email", email);
-                comando.Parameters.AddWithValue("@senha", senha);
-                comando.Parameters.AddWithValue("@celular", celular);
-                comando.Parameters.AddWithValue("@lembretesenha", lembretesenha);
-
-                check = false;
-                try
+                // checar se senha e email tem caracteres minimos
+                if (senha.Length >= 5 && email.Length >= 3)
                 {
-                    comando.Connection = conect.Conectar();
-                    comando.ExecuteNonQuery();
-                    conect.Desconectar();
-                    this.mensagem = "Cadastrado com sucesso!";
-                    
-                    check = true;
-                }
+                    // checar se senha é igual a confirmar senha
+                    if (senha.Equals(confirmarsenha))
+                    {
+                        comando.CommandText = "insert into funcionario(email, senha, celular, lembretesenha)values(@email, @senha, @celular, @lembretesenha);";
+                        comando.Parameters.AddWithValue("@email", email);
+                        comando.Parameters.AddWithValue("@senha", senha);
+                        comando.Parameters.AddWithValue("@celular", celular);
+                        comando.Parameters.AddWithValue("@lembretesenha", lembretesenha);
 
-                catch (SqlException)
-                {
-                    this.mensagem = "Erro com o banco de dados!";
+                        check = false;
+                        try
+                        {
+                            comando.Connection = conect.Conectar();
+                            comando.ExecuteNonQuery();
+                            conect.Desconectar();
+                            this.mensagem = "Cadastrado com sucesso!";
+
+                            check = true;
+                        }
+
+                        catch (SqlException)
+                        {
+                            this.mensagem = "Erro com o banco de dados!";
+                        }
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("As senhas devem ser iguais!");
+                    }
                 }
+                else
+                {
+                    MessageBox.Show("Por favor, use uma senha com mais de 5 caracteres!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, não deixe campos em branco!");
             }
             return mensagem;
         }
